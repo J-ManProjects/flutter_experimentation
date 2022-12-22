7f6eb3faa4a1fd177a78352bef3d8e87e7622aab
Jacques-PC <jmalan1995@gmail.com>
2022-12-22 17:40:40 +0200

    Very basic sliding animation working

--- lib/pages/animations.dart
+++ lib/pages/animations.dart
@@ -8,9 +8,61 @@ class Animations extends StatefulWidget {
   State<Animations> createState() => _AnimationsState();
 }
 
-class _AnimationsState extends State<Animations> {
+class _AnimationsState extends State<Animations>
+    with SingleTickerProviderStateMixin
+{
+  late AnimationController controller;
+  late Animation<Offset> offset;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    offset = Tween(
+      begin: Offset(-1.0, 0.0),
+      end: Offset(1.0, 0.0),
+    ).animate(controller);
+  }
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animations"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SlideTransition(
+            position: offset,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2,
+                  color: Colors.black54,
+                ),
+                color: Colors.purple,
+              ),
+              width: 200,
+              height: 20,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
