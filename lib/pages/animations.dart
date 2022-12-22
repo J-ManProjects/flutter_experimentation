d8f1092bf15f5f388764d6bb56f662f641c9ecae
Jacques-PC <jmalan1995@gmail.com>
2022-12-22 18:02:25 +0200

    Container dynamically slides from end to end of screen

--- lib/pages/animations.dart
+++ lib/pages/animations.dart
@@ -22,23 +22,38 @@ class _AnimationsState extends State<Animations>
       duration: Duration(seconds: 2),
       vsync: this,
     )..repeat(reverse: true);
-
-    offset = Tween(
-      begin: Offset(-1.0, 0.0),
-      end: Offset(1.0, 0.0),
-    ).animate(controller);
   }
 
 
   @override
   void dispose() {
     super.dispose();
-    controller.dispose();
+    try {
+      controller.dispose();
+    } catch (e) {
+      print(e);
+    }
   }
 
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    print("Screen width = $screenWidth");
+    print("Screen height = $screenHeight");
+
+    // The container width.
+    double width = 200;
+
+    // The horizontal end point for the animation.
+    double end = (screenWidth) / width;
+
+    // The offset tween animation.
+    offset = Tween<Offset>(
+      begin: Offset(-1.0, 0.0),
+      end: Offset(end, 0.0),
+    ).animate(controller);
 
     return Scaffold(
       appBar: AppBar(
@@ -57,7 +72,7 @@ class _AnimationsState extends State<Animations>
                 ),
                 color: Colors.purple,
               ),
-              width: 200,
+              width: width,
               height: 20,
             ),
           ),
