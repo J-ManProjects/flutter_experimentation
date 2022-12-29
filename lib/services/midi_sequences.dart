514a3813f54790450996ba0e8a215f5e4765e2d4
Jacques-PC <jmalan1995@gmail.com>
2022-12-29 02:32:58 +0200

    Improved the Ode to Joy sequence

--- lib/services/midi_sequences.dart
+++ lib/services/midi_sequences.dart
@@ -4,6 +4,24 @@ import "package:flutter_experimentation/services/notes.dart";
 class Sequence {
 
 
+  // Add a repeated sequence.
+  static void addRepeatedSequence({
+    required List<int> sequence,
+    required int count,
+    int pitch = 0,
+    bool startZero = true,
+  }) {
+    if (startZero) {
+      sequence.add(0);
+    } else {
+      sequence.add(pitch);
+    }
+    for (int i = 1; i < count; i++) {
+      sequence.add(pitch);
+    }
+  }
+
+
   // The sequence of the complete chromatic scale.
   static List<int> chromaticScale({int count = 20}) {
     List<int> sequence = [];
@@ -26,195 +44,489 @@ class Sequence {
     // Part 1:
 
     // F#4
-    int count = 41;
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(66);
-    }
+    int count = 40;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
 
     // F#4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(66);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
 
     // G4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(67);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 67,
+    );
 
     // A4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(69);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 69,
+    );
 
     // A4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(69);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 69,
+    );
 
     // G4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(67);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 67,
+    );
 
     // F#4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(66);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
 
     // E4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(64);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
 
     // D4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(62);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
 
     // D4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(62);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
 
     // E4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(64);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
 
     // F#4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(66);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
 
     // F#4
-    count = 62;
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(66);
-    }
+    count = 60;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
 
     // E4
     count = 20;
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(64);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
 
     // E4
-    count = 83;
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(64);
-    }
+    count = 40;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
+
+    // Rest.
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+    );
 
 
     // Part 2:
 
     // F#4
-    count = 41;
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(66);
-    }
+    count = 40;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
 
     // F#4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(66);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
 
     // G4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(67);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 67,
+    );
 
     // A4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(69);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 69,
+    );
 
     // A4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(69);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 69,
+    );
 
     // G4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(67);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 67,
+    );
 
     // F#4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(66);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
 
     // E4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(64);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
 
     // D4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(62);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
 
     // D4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(62);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
 
     // E4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(64);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
 
     // F#4
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(66);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
 
     // E4
-    count = 62;
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(64);
-    }
+    count = 60;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
 
     // D4
     count = 20;
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(62);
-    }
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
 
     // D4
-    count = 83;
-    sequence.add(0);
-    for (int i = 0; i < count; i++) {
-      sequence.add(62);
-    }
+    count = 40;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
+
+    // Rest.
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+    );
+
+
+    // Part 3:
+
+    // E4
+    count = 40;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
+
+    // E4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
+
+    // F#4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
+
+    // D4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
+
+    // E4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
+
+    // F#4
+    count = 20;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
+
+    // G4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 67,
+    );
+
+    // F#4
+    count = 40;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
+
+    // D4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
+
+    // E4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
+
+    // F#4
+    count = 20;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
+
+    // G4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 67,
+    );
+
+    // F#4
+    count = 40;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
+
+    // E4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
+
+    // D4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
+
+    // E4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
+
+    // A3
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 57,
+    );
+
+    // Rest.
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+    );
+
+
+    // Part 2:
+
+    // F#4
+    count = 40;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
+
+    // F#4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
+
+    // G4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 67,
+    );
+
+    // A4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 69,
+    );
+
+    // A4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 69,
+    );
+
+    // G4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 67,
+    );
+
+    // F#4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
+
+    // E4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
+
+    // D4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
+
+    // D4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
+
+    // E4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
+
+    // F#4
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 66,
+    );
+
+    // E4
+    count = 60;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 64,
+    );
+
+    // D4
+    count = 20;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
+
+    // D4
+    count = 40;
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+      pitch: 62,
+    );
+
+    // Rest.
+    Sequence.addRepeatedSequence(
+      sequence: sequence,
+      count: count,
+    );
+
 
     return sequence;
   }
