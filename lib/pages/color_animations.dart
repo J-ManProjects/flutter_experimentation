e3e855d10aa7b20c4445daa8bcf472d86c14761d
Jacques-PC <jmalan1995@gmail.com>
2022-12-29 19:17:50 +0200

    Basic colour changing working

--- lib/pages/color_animations.dart
+++ lib/pages/color_animations.dart
@@ -8,9 +8,69 @@ class ColorAnimations extends StatefulWidget {
   State<ColorAnimations> createState() => _ColorAnimationsState();
 }
 
-class _ColorAnimationsState extends State<ColorAnimations> {
+class _ColorAnimationsState extends State<ColorAnimations>
+    with TickerProviderStateMixin
+{
+  late AnimationController controller;
+  late Animation<Color>? animation;
+  late bool reverse;
+
+
+  @override
+  void initState() {
+    reverse = false;
+
+    // Initialise the controller.
+    controller = AnimationController(
+      duration: Duration.zero,
+      vsync: this,
+    );
+
+    // Initialise the color(s).
+    animation = Tween<Color>(
+      begin: Colors.indigo,
+      end: Colors.amber,
+    ).animate(controller);
+
+    // Add a status listener to the controller.
+    controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        print("Completed");
+        reverse = !reverse;
+      } else if (status == AnimationStatus.dismissed) {
+        print("Dismissed");
+        reverse = !reverse;
+      }
+    });
+
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Color Changing Animations"),
+      ),
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: animation?.value,
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                if (reverse) {
+                  controller.reverse();
+                } else {
+                  controller.forward();
+                }
+              });
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
