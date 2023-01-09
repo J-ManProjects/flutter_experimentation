9be39d98d257defec775786fec476404aa5af829
Jacques-PC <jmalan1995@gmail.com>
2023-01-09 16:11:10 +0200

    Added navigation bar to show previous and next 64 bytes

--- lib/pages/bin_viewer.dart
+++ lib/pages/bin_viewer.dart
@@ -1,4 +1,5 @@
 import "dart:io";
+import "dart:math";
 import "dart:typed_data";
 import "package:flutter/material.dart";
 import "package:flutter_experimentation/services/wav_file.dart";
@@ -19,11 +20,8 @@ class _BinViewerState extends State<BinViewer> {
   late Uint8List data;
   late Map<int, TableColumnWidth> columnWidths;
   late List<TableRow> headings;
-  late List<TableRow> content;
-  late String ascii;
-  late String hex;
   late Uint8List subData;
-  late String bytes;
+  late int offset;
 
 
   @override
@@ -36,7 +34,7 @@ class _BinViewerState extends State<BinViewer> {
     headings = [
       TableRow(
         children: <Widget>[
-          headingCell("Index"),
+          headingCell("Offset"),
           headingCell("Hex"),
           headingCell("ASCII"),
           headingCell("Uint8"),
@@ -56,8 +54,8 @@ class _BinViewerState extends State<BinViewer> {
       5: FlexColumnWidth(3),
     };
 
-    // Initialise the content list.
-    content = [];
+    // The data offset to start from.
+    offset = 0;
 
     super.initState();
   }
@@ -65,48 +63,8 @@ class _BinViewerState extends State<BinViewer> {
 
   @override
   Widget build(BuildContext context) {
-
-    // Configure the content.
-    content = List.generate(100, (index) {
-      subData = data.sublist(index*2, index*2+2);
-
-      // Attempt to get the ASCII value.
-      ascii = WavFile.bytesToAscii(subData, allowInvalid: true);
-
-      // Replace all escape sequences.
-      ascii = ascii.replaceAll(RegExp("\t"), "\\t");
-      ascii = ascii.replaceAll(RegExp("\b"), "\\b");
-      ascii = ascii.replaceAll(RegExp("\n"), "\\n");
-      ascii = ascii.replaceAll(RegExp("\r"), "\\r");
-      ascii = ascii.replaceAll(RegExp("\f"), "\\f");
-
-      // Represent with quotation marks.
-      ascii = "\"$ascii\"";
-
-      // Get the hexadecimal value.
-      hex = subData[0].toRadixString(16).padLeft(2, "0");
-      hex += " ";
-      hex += subData[1].toRadixString(16).padLeft(2, "0");
-      hex = hex.toUpperCase();
-
-      // Get the two bytes.
-      bytes = "${subData[0]}".padLeft(3);
-      bytes += " | ";
-      bytes += "${subData[1]}".padLeft(3);
-
-      return TableRow(
-        children: <Widget>[
-          contentCell("${index*2}"),
-          contentCell(hex),
-          contentCell(ascii),
-          contentCell(bytes),
-          contentCell("${WavFile.bytesToUint16(subData)}"),
-          contentCell("${WavFile.bytesToInt16(subData)}"),
-        ],
-      );
-    });
-
     return Scaffold(
+      bottomNavigationBar: navigationBar(),
       body: SafeArea(
         child: Column(
           children: <Widget>[
@@ -128,7 +86,7 @@ class _BinViewerState extends State<BinViewer> {
                       width: 2,
                       color: Theme.of(context).dividerColor,
                     ),
-                    children: content,
+                    children: getContent(),
                   ),
                 ),
               ),
@@ -138,6 +96,7 @@ class _BinViewerState extends State<BinViewer> {
       ),
     );
   }
+
 
   // Returns the table heading cell widget.
   Widget headingCell(String text) {
@@ -169,6 +128,87 @@ class _BinViewerState extends State<BinViewer> {
           ),
         ),
       ),
+    );
+  }
+
+
+  // Load all specified contents.
+  List<TableRow> getContent() {
+    String ascii;
+    String hex;
+    String bytes;
+    int start;
+
+    // Configure the size.
+    int size = min(32, data.length~/2 - offset);
+
+    // Configure the content.
+    return List.generate(size, (index) {
+      start = offset+index*2;
+      subData = data.sublist(start, start+2);
+
+      // Attempt to get the ASCII value.
+      ascii = WavFile.bytesToAscii(subData, allowInvalid: true);
+
+      // Replace all escape sequences.
+      ascii = ascii.replaceAll(RegExp("\b"), "\\b");
+      ascii = ascii.replaceAll(RegExp("\f"), "\\f");
+      ascii = ascii.replaceAll(RegExp("\n"), "\\n");
+      ascii = ascii.replaceAll(RegExp("\r"), "\\r");
+      ascii = ascii.replaceAll(RegExp("\t"), "\\t");
+      ascii = ascii.replaceAll(RegExp("\v"), "\\v");
+
+      // Represent with quotation marks.
+      ascii = "\"$ascii\"";
+
+      // Get the hexadecimal value.
+      hex = subData[0].toRadixString(16).padLeft(2, "0");
+      hex += " ";
+      hex += subData[1].toRadixString(16).padLeft(2, "0");
+      hex = hex.toUpperCase();
+
+      // Get the two bytes.
+      bytes = "${subData[0]}".padLeft(3);
+      bytes += " | ";
+      bytes += "${subData[1]}".padLeft(3);
+
+      return TableRow(
+        children: <Widget>[
+          contentCell("$start"),
+          contentCell(hex),
+          contentCell(ascii),
+          contentCell(bytes),
+          contentCell("${WavFile.bytesToUint16(subData)}"),
+          contentCell("${WavFile.bytesToInt16(subData)}"),
+        ],
+      );
+    });
+  }
+
+
+  // The bottom navigation bar.
+  Widget navigationBar() {
+    return NavigationBar(
+      height: 42,
+      destinations: <Widget>[
+        TextButton(
+          onPressed: (offset > 0) ? () {
+            setState(() {
+              offset -= 64;
+            });
+          } : null,
+          child: Text("Previous 64 bytes"),
+        ),
+        TextButton(
+          onPressed: (offset + 64 < data.length) ? () {
+            setState(() {
+              offset += 64;
+            });
+          } : null,
+          child: Text("Next 64 bytes"),
+        ),
+      ],
+
     );
   }
 
